9cdf5711b90f69690299cd745669381f26bf1484
CSEAlpha <74802730+CSEAlpha@users.noreply.github.com>
2023-04-17 21:46:53 +0200

    Update Drink.java

--- src/main/java/com/example/bigmac/Drink.java
+++ src/main/java/com/example/bigmac/Drink.java
@@ -1,4 +1,25 @@
 package com.example.bigmac;
 
 public class Drink {
+    String name;
+    double price;
+    boolean isAlcoholic;
+
+    public Drink(String name, double price, boolean isAlcoholic) {
+        this.name = name;
+        this.price = price;
+        this.isAlcoholic = isAlcoholic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isAlcoholic() {
+        return isAlcoholic;
+    }
 }
