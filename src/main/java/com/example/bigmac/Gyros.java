676470fcbd3b48ff55ea87ec26dc2efdd3bbf46e
CSEAlpha <74802730+CSEAlpha@users.noreply.github.com>
2023-04-17 21:45:11 +0200

    Update Gyros.java

--- src/main/java/com/example/bigmac/Gyros.java
+++ src/main/java/com/example/bigmac/Gyros.java
@@ -1,4 +1,69 @@
 package com.example.bigmac;
 
 public class Gyros {
+    String meat;
+    boolean hasTomatoes;
+    boolean hasOnions;
+    boolean hasLettuce;
+    boolean hasTzatziki;
+    double price;
+
+    public Gyros(String meat, boolean hasTomatoes, boolean hasOnions, boolean hasLettuce, boolean hasTzatziki) {
+        this.meat = meat;
+        this.hasTomatoes = hasTomatoes;
+        this.hasOnions = hasOnions;
+        this.hasLettuce = hasLettuce;
+        this.hasTzatziki = hasTzatziki;
+        this.price = calculatePrice();
+    }
+
+    private double calculatePrice() {
+        double basePrice = 5.0;
+        double meatPrice = 0.0;
+        if (meat.equals("Chicken")) {
+            meatPrice = 1.0;
+        } else if (meat.equals("Lamb")) {
+            meatPrice = 2.0;
+        } else if (meat.equals("Beef")) {
+            meatPrice = 2.5;
+        }
+        double toppingsPrice = 0.0;
+        if (hasTomatoes) {
+            toppingsPrice += 0.5;
+        }
+        if (hasOnions) {
+            toppingsPrice += 0.5;
+        }
+        if (hasLettuce) {
+            toppingsPrice += 0.3;
+        }
+        if (hasTzatziki) {
+            toppingsPrice += 0.7;
+        }
+        return basePrice + meatPrice + toppingsPrice;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+
+    public boolean hasTomatoes() {
+        return hasTomatoes;
+    }
+
+    public boolean hasOnions() {
+        return hasOnions;
+    }
+
+    public boolean hasLettuce() {
+        return hasLettuce;
+    }
+
+    public boolean hasTzatziki() {
+        return hasTzatziki;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
