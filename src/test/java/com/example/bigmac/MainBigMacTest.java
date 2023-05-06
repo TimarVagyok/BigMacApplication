a7e4800fa98d95f74818907d05b9af340e15bbda
EthanGaoTianyu224 <ethanGaoTianyu@mailbox.unideb.hu>
2023-05-06 13:13:58 +0200

    some update

---
+++ src/test/java/com/example/bigmac/MainBigMacTest.java
@@ -0,0 +1,22 @@
+package com.example.bigmac;
+
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainBigMacTest {
+
+    @Test
+    void TestifstartisThowing() {
+        
+    }
+
+    @Test
+    void setRoot() {
+    }
+
+}
